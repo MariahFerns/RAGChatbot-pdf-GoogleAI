37876ccaa3a98bacbc27043df5b4714dd812335b
MariahFerns <165193476+MariahFerns@users.noreply.github.com>
2024-04-05 17:47:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,10 +18,6 @@
 
 # +
 # Import required libraries
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from PyPDF2 import PdfReader
 import streamlit as st
 import google.generativeai as genai
